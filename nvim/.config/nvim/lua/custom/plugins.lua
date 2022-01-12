cd7ae1f33e515b10dcd66ff319b430e617837b64
Elijah Manor <emanor@planview.com>
2022-01-12 11:51:53 -0600

    Update PackerSync on change to plugins.lua

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -77,6 +77,6 @@ require("packer").startup({
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
