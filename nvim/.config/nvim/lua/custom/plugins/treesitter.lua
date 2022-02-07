615d36a2d398613e2831fbfaa89a1892eb021705
Elijah Manor <elijah.manor@gmail.com>
2022-02-06 23:21:11 -0600

    Update treesitter with indent and commentstring

--- nvim/.config/nvim/lua/custom/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -7,4 +7,10 @@ require'nvim-treesitter.configs'.setup {
     disable = {},
     additional_vim_regex_highlighting = false,
   },
+  indent = { -- is this native or a plugin?
+	  enable = true
+  },
+  context_commentstring = { -- this is an extension... TODO, install
+	  enable = true
+  }
 }
