29a2cda8146237a446c32a937940fd75e77197c3
Elijah Manor <elijah.manor@gmail.com>
2023-04-12 20:40:57 -0500

    move file-browser into telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,10 +2,24 @@ local Util = require("lazyvim.util")
 
 return {
   {
-      "nvim-telescope/telescope.nvim",
-  cmd = "Telescope",
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
     keys = {
       { "<leader>fR", Util.telescope("resume"), desc = "Resume" },
     }
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    keys = {
+      {
+        "<leader>sB",
+        ":Telescope file_browser file_browser path=%:p:h=%:p:h<cr>",
+        desc = "Browse Files",
+      },
+    },
+    config = function(_, opts)
+      print(vim.inspect(opts))
+      require("telescope").load_extension("file_browser")
+    end,
   }
 }
