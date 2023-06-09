6ad2aa0a35711845ff0cc2356c582a2307422baf
Elijah Manor <elijah.manor@gmail.com>
2023-06-09 15:13:03 -0500

    setup config for isRecording mode

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,12 +8,13 @@ function scheme_for_appearance(appearance)
 	end
 end
 
-return {
+local config = {
 	color_scheme = "Catppuccin Frappe",
 	-- color_scheme = "Catppuccin Latte",
 	-- color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	font = wezterm.font_with_fallback({
 		"JetBrains Mono",
+		-- "Rec Mono Duotone",
 		{ family = "Symbols Nerd Font Mono", scale = 0.75 },
 	}),
 	window_background_opacity = 0.9,
@@ -21,14 +22,24 @@ return {
 	force_reverse_video_cursor = true,
 	window_decorations = "RESIZE",
 	use_cap_height_to_scale_fallback_fonts = true,
-	-- font_size = 14,
-	font_size = 22,
+	font_size = 18,
 	scrollback_lines = 5000,
 	hide_tab_bar_if_only_one_tab = true,
 	window_close_confirmation = "NeverPrompt",
-	window_padding = { left = 4, right = 4, top = 4, bottom = 4 },
+	window_padding = { left = 20, right = 5, top = 10, bottom = 0 },
 	send_composed_key_when_left_alt_is_pressed = false,
 	send_composed_key_when_right_alt_is_pressed = true,
 	adjust_window_size_when_changing_font_size = false,
 	warn_about_missing_glyphs = false,
 }
+
+local isRecording = true
+if isRecording then
+	config.color_scheme = "tokyonight_moon"
+	config.font_size = 24
+	config.window_padding = { left = 20, right = 5, top = 10, bottom = 0 }
+	config.window_background_opacity = 1.0
+	config.macos_window_background_blur = 0
+end
+
+return config
