4cf0d3195b9a8e330d8e7f00e288770ade51313b
Elijah Manor <elijah.manor@gmail.com>
2023-06-20 08:51:45 -0500

    tmux updates

--- tmux/.tmux/watson.sh
+++ tmux/.tmux/watson.sh
@@ -1,13 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 function mrwatson() {
 	local status=""
 	if [[ "$(watson status)" == "No project started." ]]; then
-		status=""
+		status=""
 	fi
 	local total=$(watson report -dcG | grep 'Total:' | sed 's/Total: //')
 	printf "%s %s" "${status}" "${total}"
 }
 
 mrwatson
-
