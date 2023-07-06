cba248ad4eb72047d442f6618a288b0d011df7e9
Elijah Manor <elijah.manor@gmail.com>
2023-07-05 20:19:38 -0500

    update telescope to have undo

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,13 +4,13 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "debugloop/telescope-undo.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
     keys = {
       { "<leader>fR", Util.telescope("resume"), desc = "Resume" },
-    }
-  },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    keys = {
       {
         "<leader>sB",
         ":Telescope file_browser file_browser path=%:p:h=%:p:h<cr>",
@@ -18,7 +18,7 @@ return {
       },
     },
     config = function(_, opts)
-      print(vim.inspect(opts))
+      require("telescope").load_extension("undo")
       require("telescope").load_extension("file_browser")
     end,
   }
